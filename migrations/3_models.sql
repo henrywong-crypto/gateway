b40fb0b27a465c8dc85db5d1149adc7b3406d27d
Henry Wong <henry.hs.wong@crypto.com>
2026-02-07 21:03:07 +0800

    Test

--- migrations/3_models.sql
+++ migrations/3_models.sql
@@ -1,7 +1,7 @@
 create table if not exists models (
     created_at timestamptz not null default now(),
     model_id uuid primary key default uuid_generate_v4(),
-    model_name varchar(255) not null unique,
+    model_arn varchar(512) not null unique,
     protected boolean not null default false,
     updated_at timestamptz not null default now()
 );
--- migrations/7_models_rename_model_name_to_model_arn.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE models RENAME COLUMN model_name TO model_arn;
-ALTER TABLE models ALTER COLUMN model_arn TYPE VARCHAR(512);
