a8d7cad288c39409586ab285b9790ae87b126c02
Henry Wong <henry.hs.wong@crypto.com>
2026-02-08 12:00:54 +0800

    Test

--- inference_profiles/src/lib.rs
+++ inference_profiles/src/lib.rs
@@ -6,7 +6,6 @@ use uuid::Uuid;
 pub async fn create_inference_profile(
     model_id: &str,
     profile_name: &str,
-    tags: Vec<(String, String)>,
     aws_region: &str,
     aws_account_id: &str,
     inference_profile_prefixes: &[String],
@@ -23,24 +22,15 @@ pub async fn create_inference_profile(
         model_id.to_string()
     };
 
-    let mut request = client
+    let response = client
         .create_inference_profile()
         .inference_profile_name(profile_name)
         .model_source(
             aws_sdk_bedrock::types::InferenceProfileModelSource::CopyFrom(copy_from),
-        );
-
-    for (key, value) in &tags {
-        request = request.tags(
-            aws_sdk_bedrock::types::Tag::builder()
-                .key(key)
-                .value(value)
-                .build()
-                .map_err(|e| anyhow::anyhow!("Failed to build tag: {}", e))?,
-        );
-    }
-
-    let response = request.send().await.map_err(|e| {
+        )
+        .send()
+        .await
+        .map_err(|e| {
         tracing::error!("Failed to create inference profile '{}': {:?}", profile_name, e);
         e
     })?;
@@ -52,31 +42,6 @@ pub async fn create_inference_profile(
     Ok(arn)
 }
 
-pub async fn create_inference_profile_record(
-    pool: &PgPool,
-    user_email: &str,
-    model_name: &str,
-    inference_profile_arn: &str,
-    inference_profile_name: &str,
-) -> Result<()> {
-    sqlx::query!(
-        r#"
-        INSERT INTO inference_profiles (user_id, model_id, inference_profile_arn, inference_profile_name)
-        SELECT u.user_id, m.model_id, $3, $4
-        FROM users u, models m
-        WHERE u.email = $1 AND m.model_name = $2
-        "#,
-        user_email.to_lowercase(),
-        model_name.to_lowercase(),
-        inference_profile_arn,
-        inference_profile_name,
-    )
-    .execute(pool)
-    .await?;
-
-    Ok(())
-}
-
 pub async fn create_and_store_inference_profile(
     pool: &PgPool,
     api_key: &str,
@@ -86,7 +51,7 @@ pub async fn create_and_store_inference_profile(
     inference_profile_prefixes: &[String],
 ) -> Result<String> {
     let profile_name = Uuid::new_v4().to_string();
-    let arn = create_inference_profile(model_name, &profile_name, vec![], aws_region, aws_account_id, inference_profile_prefixes).await?;
+    let arn = create_inference_profile(model_name, &profile_name, aws_region, aws_account_id, inference_profile_prefixes).await?;
 
     sqlx::query!(
         r#"
